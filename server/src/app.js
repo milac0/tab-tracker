0f8632f8cdd3f19200728e6f20a5af94589348a0
unknown <milac0@hotmail.com>
2019-12-16 12:57:04 +0100

    setup express server

---
+++ server/src/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(morgan('combine'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/status', (req, res) => {
+    res.send({msg: 'hej'})
+})
+
+app.listen(3000, () => console.log('server started..'))
